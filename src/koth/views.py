8481d058f8451db664bba974c458ecae16b7333e
F11snipe <f11snipe@gmail.com>
2022-06-29 15:29:50 -0600

    Basic working filters

--- src/koth/views.py
+++ src/koth/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
 from koth.models import Box, Player, Game, GamePlayer
 from koth.serializers import BoxSerializer, PlayerSerializer, GameSerializer, GamePlayerSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, filters as bfilters
+from rest_framework_json_api import filters
+from rest_framework_json_api import django_filters
 
 class BoxViewSet(viewsets.ModelViewSet):
     queryset = Box.objects.all()
     serializer_class = BoxSerializer
+    filter_backends = (filters.QueryParameterValidationFilter, filters.OrderingFilter,
+                        django_filters.DjangoFilterBackend, bfilters.SearchFilter)
+    filterset_fields = {
+        'id': ('exact', 'lt', 'gt', 'gte', 'lte', 'in'),
+        'os': ('icontains', 'iexact', 'contains'),
+        'title': ('icontains', 'iexact', 'contains')
+    }
+    search_fields = ('id', 'os', 'title',)
 
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
+    filter_backends = (filters.QueryParameterValidationFilter, filters.OrderingFilter,
+                        django_filters.DjangoFilterBackend, bfilters.SearchFilter)
+    filterset_fields = {
+        'id': ('exact', 'lt', 'gt', 'gte', 'lte', 'in'),
+        'level': ('exact', 'lt', 'gt', 'gte', 'lte', 'in'),
+        'username': ('icontains', 'iexact', 'contains')
+    }
+    search_fields = ('id', 'level', 'username',)
 
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
